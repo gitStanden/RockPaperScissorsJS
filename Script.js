62f62e596a432df8af487e0b10eb029fd67d7116
Alex Standen <alexis.standen3@gmail.com>
2022-08-23 13:42:21 +0100

    Added Helper Functions to break up playRound

--- Script.js
+++ Script.js
@@ -4,31 +4,43 @@ function getComputerChoice(){
     return rps[randomChoice];
 };
 
+function playerRock(computerSelection){
+    if(computerSelection == 'ROCK'){
+        console.log('Computer chose ROCK\nRock vs Rock - TIE!');
+    } else if (computerSelection == 'PAPER'){
+        console.log('Computer chose PAPER\nPaper beats Rock - YOU LOSE!');
+    } else if (computerSelection == 'SCISSORS'){
+        console.log('Computer chose SCISSORS\nRock beats Scissors - YOU WIN!');
+    };
+};
+
+function playerPaper(computerSelection){
+    if(computerSelection == 'ROCK'){
+        console.log('Computer chose ROCK\n Paper beats Rock - YOU WIN!');
+    } else if (computerSelection == 'PAPER'){
+        console.log('Computer chose PAPER\nPaper vs Paper - TIE!');
+    } else if (computerSelection == 'SCISSORS'){
+        console.log('Computer chose SCISSORS\nScissors beats Paper - YOU LOSE!');
+    };
+};
+
+function playerScissors(computerSelection){
+    if(computerSelection == 'ROCK'){
+        console.log('Computer chose ROCK\nRock beats Scissors - YOU LOSE!');
+    } else if (computerSelection == 'PAPER'){
+        console.log('Computer chose PAPER\nScissors beats Paper - YOU WIN!');
+    } else if (computerSelection == 'SCISSORS'){
+        console.log('Computer chose SCISSORS\nScissors vs Scissors - TIE!');
+    };
+};
+
 function playRound(playerSelection,computerSelection){
     if(playerSelection == 'ROCK'){
-        if(computerSelection == 'ROCK'){
-            console.log('Computer chose ROCK\nRock vs Rock - TIE!');
-        } else if (computerSelection == 'PAPER'){
-            console.log('Computer chose PAPER\nPaper beats Rock - YOU LOSE!');
-        } else if (computerSelection == 'SCISSORS'){
-            console.log('Computer chose SCISSORS\nRock beats Scissors - YOU WIN!');
-        };
+        playerRock(computerSelection);
     }else if(playerSelection == 'PAPER'){
-        if(computerSelection == 'ROCK'){
-            console.log('Computer chose ROCK\n Paper beats Rock - YOU WIN!');
-        } else if (computerSelection == 'PAPER'){
-            console.log('Computer chose PAPER\nPaper vs Paper - TIE!');
-        } else if (computerSelection == 'SCISSORS'){
-            console.log('Computer chose SCISSORS\nScissors beats Paper - YOU LOSE!');
-        };
+        playerPaper(computerSelection);
     }else if(playerSelection == 'SCISSORS'){
-        if(computerSelection == 'ROCK'){
-            console.log('Computer chose ROCK\nRock beats Scissors - YOU LOSE!');
-        } else if (computerSelection == 'PAPER'){
-            console.log('Computer chose PAPER\nScissors beats Paper - YOU WIN!');
-        } else if (computerSelection == 'SCISSORS'){
-            console.log('Computer chose SCISSORS\nScissors vs Scissors - TIE!');
-        };
+        playerScissors(computerSelection);
     };
 };
 
