310a50455788e5d67d1e22d2cbbec6fb5c76b382
Alex Standen <alexis.standen3@gmail.com>
2022-08-23 13:34:29 +0100

    Added playRound Function

--- Script.js
+++ Script.js
@@ -1,12 +1,38 @@
 function getComputerChoice(){
-    const rps = ['Rock', 'Paper', 'Scissors'];
+    const rps = ['ROCK', 'PAPER', 'SCISSORS'];
     const randomChoice = Math.floor(Math.random() * rps.length);
-    console.log(rps[randomChoice]);
+    return rps[randomChoice];
 };
 
-function playerSelection(){
-    const selection = prompt("Please enter \'Rock\', \'Paper\' or \'Scissors\': ").toLowerCase();
-    console.log(selection);
+function playRound(playerSelection,computerSelection){
+    if(playerSelection == 'ROCK'){
+        if(computerSelection == 'ROCK'){
+            console.log('Computer chose ROCK\nRock vs Rock - TIE!');
+        } else if (computerSelection == 'PAPER'){
+            console.log('Computer chose PAPER\nPaper beats Rock - YOU LOSE!');
+        } else if (computerSelection == 'SCISSORS'){
+            console.log('Computer chose SCISSORS\nRock beats Scissors - YOU WIN!');
+        };
+    }else if(playerSelection == 'PAPER'){
+        if(computerSelection == 'ROCK'){
+            console.log('Computer chose ROCK\n Paper beats Rock - YOU WIN!');
+        } else if (computerSelection == 'PAPER'){
+            console.log('Computer chose PAPER\nPaper vs Paper - TIE!');
+        } else if (computerSelection == 'SCISSORS'){
+            console.log('Computer chose SCISSORS\nScissors beats Paper - YOU LOSE!');
+        };
+    }else if(playerSelection == 'SCISSORS'){
+        if(computerSelection == 'ROCK'){
+            console.log('Computer chose ROCK\nRock beats Scissors - YOU LOSE!');
+        } else if (computerSelection == 'PAPER'){
+            console.log('Computer chose PAPER\nScissors beats Paper - YOU WIN!');
+        } else if (computerSelection == 'SCISSORS'){
+            console.log('Computer chose SCISSORS\nScissors vs Scissors - TIE!');
+        };
+    };
 };
 
-playerSelection();
+
+let playerSelection = prompt("Please enter \'ROCK\', \'PAPER\' or \'SCISSORS\': ").toUpperCase();
+let computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
